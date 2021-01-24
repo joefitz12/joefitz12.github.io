9c73338b896756d9f1b080fb64491f70cfd6e8df
Joe Fitzpatrick <jefitzpa@gmail.com>
2021-01-24 14:23:11 -0600

    changes for mobile

--- interface.js
+++ interface.js
@@ -102,9 +102,10 @@ $(document).ready(function(){
             mainCanvas.width = width;
             mainCanvas.height = height;
         }
-    
+        console.log(e.touches[0]);
         let startingX = e.layerX ? e.layerX : e.touches[0].clientX;
-        let startingY = e.layerY ? e.layerY : e.touches[0].clientY + document.querySelectorAll('.header')[0].offsetHeight;
+        let startingY = e.layerY ? e.layerY : e.touches[0].clientY;
+        // let startingY = e.layerY ? e.layerY : e.touches[0].clientY - document.querySelectorAll('.header')[0].offsetHeight;
         // alert('e.touches[0].clientY' + e.touches[0].clientY);
         // alert("document.querySelectorAll('.header')[0].offsetHeight" + document.querySelectorAll('.header')[0].offsetHeight);
         // alert('startingY' + startingY);
